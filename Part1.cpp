6a0ca6b92c7a001c8b647d9dcf12db64a4b7d46f
joeyjchen@hotmail.com <joeyjchen@hotmail.com>
2021-11-13 12:00:06 -0500

    Finalized Part 1

--- Part1.cpp
+++ Part1.cpp
@@ -2,19 +2,53 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdio.h>
+#include <cctype>
+
+
+int buttonMap[] = {2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9};
+double buttonDelay[] = {0, 0.25, 0.50, 0, 0.25, 0.50, 0, 0.25, 0.50,0, 0.25, 0.50,0, 0.25, 0.50,0, 0.25, 0.50, 0.75, 0, 0.25, 0.50, 0, 0.25, 0.50, 0.75};
+
+double timeForLetter(char letter, char previousLetter){
+    double time = 0;
+
+    if (isupper(letter)){ // Add 2 seconds to the time if it is capitalized 
+        letter += 32;
+        time += 2;
+    }
+    time += buttonDelay[int(letter)-97]; // Add the time to reach the letter
+
+    if (previousLetter != '\0' && buttonMap[int(tolower(previousLetter))-97] == buttonMap[int(letter)-97]){ 
+        time += 0.5; // If its the same button cluster, then 0.5 sec
+    }else{
+        time += 0.25; //If its a new button cluster, then only 0.25 sec
+    }
+
+    if (previousLetter == '\0'){ // If first letter, then no first button delay.
+        time -= 0.25; 
+    }
+    return time;
+}
+
 
 int main () {
     std::string line;
+    char previousLetter = '\0';
+    double time = 0;
     std::ifstream myfile;
     myfile.open ("PACKAGE/Test1.txt");
 
     if(myfile.is_open()){
-        while ( std::getline (myfile,line)){
-        std::cout << line << " -> ";
-
-        
-
+        while (myfile >> line){
+            previousLetter = '\0';
+            time = 0;
+            for (int i = 0; i<line.size(); i++){
+                time += timeForLetter(line[i], previousLetter);
+                previousLetter = line[i];
+            }
+            std::cout << line << " -> " << time << std::endl;
         }
+
         myfile.close();
     }else{
         std::cout << "File could not be openned or could not be found" << std::endl;
